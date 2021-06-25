645af61ffb32c415d725d0093391f47a28cfa980
Neeltjevantilburg <n.vantilburg@student.fontys.nl>
2021-06-25 10:29:27 +0200

    Neeltje update

--- js/main.js
+++ js/main.js
@@ -2,141 +2,174 @@ var canvas = document.getElementById("canvas");
 var engine = new BABYLON.Engine(canvas, true);
 
 // here the doc for Load function: //doc.babylonjs.com/typedoc/classes/babylon.sceneloader#load
-BABYLON.SceneLoader.Load("", "assets/room.babylon", engine, function (scene) {
-  // Need a free camera for collisions
-  var camera = new BABYLON.UniversalCamera(
-    "FreeCamera",
-    new BABYLON.Vector3(0, 10, 0),
-    scene
-  );
-  camera.attachControl(canvas, true);
-  camera.speed = 0.2;
+BABYLON.SceneLoader.Load(
+  "",
+  "assets/sanderroom.babylon",
+  engine,
+  function (scene) {
+    // Need a free camera for collisions
+    var camera = new BABYLON.UniversalCamera(
+      "FreeCamera",
+      new BABYLON.Vector3(-40, 10, 10),
+      scene
+    );
+    camera.attachControl(canvas, true);
+    camera.speed = 0.6;
 
-  //Set gravity for the scene (G force like, on Y-axis)
-  scene.gravity = new BABYLON.Vector3(0, -0.9, 0);
+    //Set gravity for the scene (G force like, on Y-axis)
+    scene.gravity = new BABYLON.Vector3(0, -0.9, 0);
 
-  // Enable Collisions
-  scene.collisionsEnabled = true;
+    // Enable Collisions
+    scene.collisionsEnabled = true;
 
-  //Then apply collisions and gravity to the active camera
-  camera.checkCollisions = true;
-  camera.applyGravity = true;
+    //Then apply collisions and gravity to the active camera
+    camera.checkCollisions = true;
+    camera.applyGravity = true;
 
-  //Set the ellipsoid around the camera (e.g. your player's size)
-  camera.ellipsoid = new BABYLON.Vector3(1, 1, 1);
-  scene.clearColor = new BABYLON.Color3(1, 1, 1);
-  scene.ambientColor = new BABYLON.Color3.White();
+    //Set the ellipsoid around the camera (e.g. your player's size)
+    camera.ellipsoid = new BABYLON.Vector3(1, 1, 1);
+    scene.clearColor = new BABYLON.Color3(1, 1, 1);
+    scene.ambientColor = new BABYLON.Color3.White();
 
-  var light = new BABYLON.HemisphericLight(
-    "hemiLight",
-    new BABYLON.Vector3(0, 0, 100),
-    scene
-  );
+    var light = new BABYLON.HemisphericLight(
+      "hemiLight",
+      new BABYLON.Vector3(1, 10, 15),
+      scene
+    );
 
-  // var dirLight = new BABYLON.DirectionalLight(
-  //   "dirLight",
-  //   new BABYLON.Vector3(1, -1, 1),
-  //   scene
-  // );
-  // dirLight.diffuse = new BABYLON.Color3(1, 1, 0.98);
-  // dirLight.position = new BABYLON.Vector3(-100, 100, -100);
+    engine.runRenderLoop(function () {
+      camera.position.y = 1;
+      scene.render();
+    });
 
-  engine.runRenderLoop(function () {
-    scene.render();
-  });
-
-  window.addEventListener("resize", function () {
-    engine.resize();
-  });
-
-
-
-  // code sander
+    window.addEventListener("resize", function () {
+      engine.resize();
+    });
 
     // scene.debugLayer.show();
 
     // ================================================= OBJECT KLIKBAAR  =================================================
 
     // zorgen dat de pagina niet refresht na het aanpassen van de visibility
-      var xFullPath = window.location.pathname;
-      var xPageName = xFullPath.substring(xFullPath.lastIndexOf("/") + 1);
-    
+    var xFullPath = window.location.pathname;
+    var xPageName = xFullPath.substring(xFullPath.lastIndexOf("/") + 1);
 
     scene.onPointerUp = (evt, pickinfo) => {
-      if (pickinfo.hit)
-        // alert("dit is een: " + pickinfo.pickedMesh.name);
-        var objectInfo = pickinfo.pickedMesh.name;
-          document.getElementById("HUD").innerHTML = "Naam object: " + objectInfo;
+      if (pickinfo.hit) var objectInfo = pickinfo.pickedMesh.name;
+      document.getElementById("HUD").innerHTML = "Naam object: " + objectInfo;
 
-          if (xPageName !== "index.html")
-          {
-            window.location.replace("../index.html");
-          }
+      if (xPageName !== "index.html") {
+        window.location.replace("../index.html");
+      }
 
-          var sword = scene.getMeshByName("Sword");
+      // zwaard = =fully klikbaar (behalve 'Cube' en 'zwaard1')
+      if (
+        objectInfo == "zwaard1" ||
+        objectInfo == "zwaard2" ||
+        objectInfo == "Icosahedron" ||
+        objectInfo == "Sphere" ||
+        objectInfo == "Cube"
+      ) {
+        $("#Sphere").modal();
+      } else {
+      }
 
-        if (objectInfo == "Cylinder.001"){
-          document.getElementById('Zwaard').style.visibility = 'visible';
-        } else {
-          document.getElementById('Zwaard').style.visibility = 'hidden';
-        }
+      // volleybal  = fully klikbaar
+      if (objectInfo == "volleybal.001") {
+        $("#volleybal").modal();
+      } else {
+      }
 
-        if (objectInfo == "Cube 2"){
-          document.getElementById('Stoel').style.visibility = 'visible';
-        } else {
-          document.getElementById('Stoel').style.visibility = 'hidden';
-        }
-    };	
+      // wereldbol  = fully klikbaar
+      if (
+        objectInfo == "wereldbol.001" ||
+        objectInfo == "Cylinder 3" ||
+        objectInfo == "Cylinder 4" ||
+        objectInfo == "Torus"
+      ) {
+        $("#Torus").modal();
+      } else {
+      }
 
+      // schilderij = fully klikbaar
+      if (
+        objectInfo == "Group_002" ||
+        objectInfo == "Group_004" ||
+        objectInfo == "schilderij.001" ||
+        objectInfo == "Group_001" ||
+        objectInfo == "Group_013" ||
+        objectInfo == "Group_003"
+      ) {
+        $("#schilderij").modal();
+      } else {
+      }
 
-     // ========================= clickable BABYLONJS OBJECT   ========================= =========================
-            
-     var urlList = [
-      "https://fontys.nl/Studeren/Aanmelden/Uitschrijven.htm",
-      "https://fontys.nl/Studeren/Aanmelden/Uitschrijven.htm",
-      "https://fontys.nl/Studeren/Aanmelden/Uitschrijven.htm",
-  ];
+      // prullenbak = fullt klikbaar
+      if (objectInfo == "prul1" || objectInfo == "prul2") {
+        $("#bin").modal();
+      } else {
+      }
 
-  var textures = [
-      "https://thumbs.dreamstime.com/b/seamless-football-pattern-soccer-ball-texture-banner-186742659.jpg",
-      "https://i.imgur.com/Pox1X97.png",
-      "https://upload.wikimedia.org/wikipedia/commons/1/10/IC_engine.JPG"
+      // glazen bol = fully klikbaar
+      if (
+        objectInfo == "klantreview.001" ||
+        objectInfo == "klantreview2" ||
+        objectInfo == "Cube.001" ||
+        objectInfo == "Cube.002" ||
+        objectInfo == "Cube.003" ||
+        objectInfo == "Cube.004"
+      ) {
+        $("#glasbol").modal();
+      } else {
+      }
 
-  ];
+      // magic formula = fully klikbaar
+      if (
+        objectInfo == "Flask_Cone" ||
+        objectInfo == "Beaker_Cylinder" ||
+        objectInfo == "lab2"
+      ) {
+        $("#lab2").modal();
+      } else {
+      }
 
-  // this is function that is called on each cylinder pick
-  var pickCylinder = function(meshEvent) {
-      var pickedMesh = meshEvent.meshUnderPointer; 
+      // dossier =  fully klikbaar
+      if (
+        objectInfo == "group_0_16768282" ||
+        objectInfo == "group_0_15277357.001" ||
+        objectInfo == "bierensdocument.001" ||
+        objectInfo == "bierensdocument2"
+      ) {
+        $("#bierensdocument").modal();
+      } else {
+      }
 
-      window.open(urlList[Number(pickedMesh.name)]);
-  }
+      // boek = fully klikbaar
+      if (
+        objectInfo == "group_0_14860437" ||
+        objectInfo == "group_0_16448250" ||
+        objectInfo == "bierensboek" ||
+        objectInfo == "group_0_16766720"
+      ) {
+        $("#bierensboek").modal();
+      } else {
+      }
 
-
-    var pikCylinder = function() {
-      window.open('https://thumbs.dreamstime.com/b/seamless-football-pattern-soccer-ball-texture-banner-186742659.jpg');
-  }
-
-  // cylinder generator with actionManagers on each
-  var angle = 0;
-  for (var index = 0; index < 3; index++) {
-      var cylinder  = BABYLON.MeshBuilder.CreateSphere(index, {radius: 0.5, height: 0.2}, scene);
-      cylinder.position.y =  Math.sin(angle) +1;
-      cylinder.position.x =  Math.cos(angle) -4;
-
-      cylinder.material = new BABYLON.StandardMaterial("mat_"+ index, scene);
-      cylinder.material.diffuseTexture = new BABYLON.Texture(textures[index]);
-      // add actionManager on each cyl
-      cylinder.actionManager = new BABYLON.ActionManager(scene);
-      // register 'pickCylinder' as the handler function for cylinder picking action.
-      cylinder.actionManager.registerAction(
-          new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger, pickCylinder)
-      );
-
-      logo.actionManager.registerAction(
-          new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger, pickCylinder)
-      );
-
-      angle += 2 * Math.PI / 3;
-  } 
+      // cola + burger = fully klikbaar
+      if (
+        objectInfo == "cola" ||
+        objectInfo == "lettuce" ||
+        objectInfo == "meat" ||
+        objectInfo == "ketchup" ||
+        objectInfo == "botton BUN" ||
+        objectInfo == "burger.001"
+      ) {
+        $("#burger").modal();
+      } else {
+      }
+    };
   }
 );
+
+//pop up fade - neel
+//
